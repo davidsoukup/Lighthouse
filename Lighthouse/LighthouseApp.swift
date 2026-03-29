d31e77a729ed29185c6540a0a4dfd6983eecbd45
davidsoukup <david@soukup.dev>
2026-03-29 17:44:15 +0200

    Initial Commit

---
+++ Lighthouse/LighthouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LighthouseApp.swift
+//  Lighthouse
+//
+//  Created by David Soukup on 29.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LighthouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
